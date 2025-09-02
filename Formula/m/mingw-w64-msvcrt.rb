a9020c68497e95a0f949c7126fc80b08c40c80e9
hi <hi@nosec.me>
2025-09-02 11:49:29 +0800

    first commit

---
+++ Formula/m/mingw-w64-msvcrt.rb
@@ -0,0 +1,238 @@
+class MingwW64Msvcrt < Formula
+  desc "Minimalist GNU for Windows and GCC cross-compilers"
+  homepage "https://sourceforge.net/projects/mingw-w64/"
+  url "https://downloads.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v13.0.0.tar.bz2"
+  sha256 "5afe822af5c4edbf67daaf45eec61d538f49eef6b19524de64897c6b95828caf"
+  license "ZPL-2.1"
+  revision 1
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?release/mingw-w64[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  bottle do
+    sha256 arm64_sequoia: "a9ea389f938684b158744296709e7bbf6eac7aaab586ce77179004f27eb27cf7"
+    sha256 arm64_sonoma:  "0dca5afe15f5e959927d43281181e5dedd801c4f0165d4868309267827e6f24a"
+    sha256 arm64_ventura: "f4d1fd8ba64fa38524639c4c57da56bd79f401a8804dcd8070440e25700f100f"
+    sha256 sonoma:        "87346a8f508721e524df7d9f6359708044c857373ddd09b43734b8dbf5f45b6a"
+    sha256 ventura:       "674057ef36abe6c677e23ee57ced865aaa07b3281675bbe53d11c74f76bdb487"
+    sha256 arm64_linux:   "fa83501cce50b4729ef8185bebe2454d703b621666c331b6dbc193e3b60d774f"
+    sha256 x86_64_linux:  "c2d0c0416f9322e0dabe14d321736f934b58a36ec36dc3b1c88770f63d5ba8cf"
+  end
+
+  # binutils searches for zstd using pkg-config
+  depends_on "pkgconf" => :build
+  # Apple's makeinfo is old and has bugs
+  depends_on "texinfo" => :build
+
+  depends_on "gmp"
+  depends_on "isl"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "zstd"
+
+  uses_from_macos "zlib"
+
+  resource "binutils" do
+    url "https://ftpmirror.gnu.org/gnu/binutils/binutils-2.44.tar.bz2"
+    mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.44.tar.bz2"
+    sha256 "f66390a661faa117d00fab2e79cf2dc9d097b42cc296bf3f8677d1e7b452dc3a"
+  end
+
+  resource "gcc" do
+    url "https://ftpmirror.gnu.org/gnu/gcc/gcc-15.2.0/gcc-15.2.0.tar.xz"
+    mirror "https://ftp.gnu.org/gnu/gcc/gcc-15.2.0/gcc-15.2.0.tar.xz"
+    sha256 "438fd996826b0c82485a29da03a72d71d6e3541a83ec702df4271f6fe025d24e"
+  end
+
+  def target_archs
+    ["i686", "x86_64"].freeze
+  end
+
+  def install
+    target_archs.each do |arch|
+      arch_dir = "#{prefix}/toolchain-#{arch}"
+      target = "#{arch}-w64-mingw32"
+
+      resource("binutils").stage do
+        args = %W[
+          --target=#{target}
+          --with-sysroot=#{arch_dir}
+          --prefix=#{arch_dir}
+          --enable-targets=#{target}
+          --disable-multilib
+          --disable-nls
+          --with-system-zlib
+          --with-zstd
+        ]
+        mkdir "build-#{arch}" do
+          system "../configure", *args
+          system "make"
+          system "make", "install"
+        end
+      end
+
+      # Put the newly built binutils into our PATH
+      ENV.prepend_path "PATH", "#{arch_dir}/bin"
+
+      mkdir "mingw-w64-headers/build-#{arch}" do
+        system "../configure", "--host=#{target}", "--prefix=#{arch_dir}/#{target}", "--with-default-msvcrt=msvcrt"
+        system "make"
+        system "make", "install"
+      end
+
+      # Create a mingw symlink, expected by GCC
+      ln_s "#{arch_dir}/#{target}", "#{arch_dir}/mingw"
+
+      # Build the GCC compiler
+      resource("gcc").stage buildpath/"gcc"
+      args = %W[
+        --target=#{target}
+        --with-sysroot=#{arch_dir}
+        --prefix=#{arch_dir}
+        --with-bugurl=#{tap.issues_url}
+        --enable-languages=c,c++,objc,obj-c++,fortran
+        --with-ld=#{arch_dir}/bin/#{target}-ld
+        --with-as=#{arch_dir}/bin/#{target}-as
+        --with-gmp=#{Formula["gmp"].opt_prefix}
+        --with-mpfr=#{Formula["mpfr"].opt_prefix}
+        --with-mpc=#{Formula["libmpc"].opt_prefix}
+        --with-isl=#{Formula["isl"].opt_prefix}
+        --with-system-zlib
+        --with-zstd
+        --disable-multilib
+        --disable-nls
+        --enable-threads=posix
+      ]
+
+      mkdir "#{buildpath}/gcc/build-#{arch}" do
+        system "../configure", *args
+        system "make", "all-gcc"
+        system "make", "install-gcc"
+      end
+
+      # Build the mingw-w64 runtime
+      args = %W[
+        CC=#{target}-gcc
+        CXX=#{target}-g++
+        CPP=#{target}-cpp
+        --host=#{target}
+        --with-sysroot=#{arch_dir}/#{target}
+        --prefix=#{arch_dir}/#{target}
+        --with-default-msvcrt=msvcrt
+      ]
+
+      case arch
+      when "i686"
+        args << "--enable-lib32" << "--disable-lib64"
+      when "x86_64"
+        args << "--disable-lib32" << "--enable-lib64"
+      end
+
+      mkdir "mingw-w64-crt/build-#{arch}" do
+        system "../configure", *args
+        # Resolves "Too many open files in system"
+        # bfd_open failed open stub file dfxvs01181.o: Too many open files in system
+        # bfd_open failed open stub file: dvxvs00563.o: Too many open files in systembfd_open
+        # https://sourceware.org/bugzilla/show_bug.cgi?id=24723
+        # https://sourceware.org/bugzilla/show_bug.cgi?id=23573#c18
+        ENV.deparallelize do
+          system "make"
+          system "make", "install"
+        end
+      end
+
+      # Build the winpthreads library
+      # we need to build this prior to the
+      # GCC runtime libraries, to have `-lpthread`
+      # available, for `--enable-threads=posix`
+      args = %W[
+        CC=#{target}-gcc
+        CXX=#{target}-g++
+        CPP=#{target}-cpp
+        --host=#{target}
+        --with-sysroot=#{arch_dir}/#{target}
+        --prefix=#{arch_dir}/#{target}
+      ]
+      mkdir "mingw-w64-libraries/winpthreads/build-#{arch}" do
+        system "../configure", *args
+        system "make"
+        system "make", "install"
+      end
+
+      args = %W[
+        --host=#{target}
+        --with-sysroot=#{arch_dir}/#{target}
+        --prefix=#{arch_dir}
+        --program-prefix=#{target}-
+      ]
+      mkdir "mingw-w64-tools/widl/build-#{arch}" do
+        system "../configure", *args
+        system "make"
+        system "make", "install"
+      end
+
+      # Finish building GCC (runtime libraries)
+      chdir "#{buildpath}/gcc/build-#{arch}" do
+        system "make"
+        system "make", "install"
+      end
+
+      # Symlinks all binaries into place
+      mkdir_p bin
+      Dir["#{arch_dir}/bin/*"].each { |f| ln_s f, bin }
+    end
+  end
+
+  test do
+    (testpath/"hello.c").write <<~C
+      #include <stdio.h>
+      #include <windows.h>
+      int main() { puts("Hello world!");
+        MessageBox(NULL, TEXT("Hello GUI!"), TEXT("HelloMsg"), 0); return 0; }
+    C
+    (testpath/"hello.cc").write <<~CPP
+      #include <iostream>
+      int main() { std::cout << "Hello, world!" << std::endl; return 0; }
+    CPP
+    (testpath/"hello.f90").write <<~FORTRAN
+      program hello ; print *, "Hello, world!" ; end program hello
+    FORTRAN
+    # https://docs.microsoft.com/en-us/windows/win32/rpc/using-midl
+    (testpath/"example.idl").write <<~MIDL
+      [
+        uuid(ba209999-0c6c-11d2-97cf-00c04f8eea45),
+        version(1.0)
+      ]
+      interface MyInterface
+      {
+        const unsigned short INT_ARRAY_LEN = 100;
+
+        void MyRemoteProc(
+            [in] int param1,
+            [out] int outArray[INT_ARRAY_LEN]
+        );
+      }
+    MIDL
+
+    ENV["LC_ALL"] = "C"
+    ENV.remove_macosxsdk if OS.mac?
+    target_archs.each do |arch|
+      target = "#{arch}-w64-mingw32"
+      outarch = (arch == "i686") ? "i386" : "x86-64"
+
+      system bin/"#{target}-gcc", "-o", "test.exe", "hello.c"
+      assert_match "file format pei-#{outarch}", shell_output("#{bin}/#{target}-objdump -a test.exe")
+
+      system bin/"#{target}-g++", "-o", "test.exe", "hello.cc"
+      assert_match "file format pei-#{outarch}", shell_output("#{bin}/#{target}-objdump -a test.exe")
+
+      system bin/"#{target}-gfortran", "-o", "test.exe", "hello.f90"
+      assert_match "file format pei-#{outarch}", shell_output("#{bin}/#{target}-objdump -a test.exe")
+
+      system bin/"#{target}-widl", "example.idl"
+      assert_path_exists testpath/"example_s.c", "example_s.c should have been created"
+    end
+  end
+end
